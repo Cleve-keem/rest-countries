a5274f64531633d5bacd63dd14ad15e72d06793e
Cleve-keem <belloharyourmidey@gmail.com>
2025-02-08 05:50:58 -0800

    add themes

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-const toggleThemeBtn = document.getElementById("toggle-theme")
+const toggleThemeBtn = document.getElementById("toggle-themes");
+
+if (!localStorage.getItem("theme")) {
+  document.documentElement.setAttribute("data-theme", "light");
+  localStorage.setItem("theme", "light")
+} else {
+  document.documentElement.setAttribute("data-theme", localStorage.getItem("theme"));
+}
+
+toggleThemeBtn.addEventListener("click", () => {
+  let themeToSet = document.documentElement.getAttribute("data-theme");
+  let currentTheme = themeToSet === "light" ? "dark" : "light";
+
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  localStorage.setItem("theme", currentTheme);
+});
