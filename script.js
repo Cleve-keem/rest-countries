e09508f9ed004743a0f6dd995b3217c9ce4ebf9d
Cleve-keem <belloharyourmidey@gmail.com>
2025-02-09 02:44:54 -0800

    added filterBy origin functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const rootElement = document.documentElement;
 const toggleThemeBtn = document.getElementById("toggle-themes");
 const countriesGrid = document.querySelector(".countries-grid");
+const dropdownOptions = document.querySelector(".drop-down-options");
 
 if (!localStorage.getItem("theme")) {
   rootElement.setAttribute("data-theme", "light");
@@ -17,12 +18,13 @@ toggleThemeBtn.addEventListener("click", () => {
   localStorage.setItem("theme", currentTheme);
 });
 
-// let currentCountries = [];
+let allCountries = [];
 
 async function fetchCountries() {
   try {
     const res = await fetch("https://restcountries.com/v3.1/all");
     const data = await res.json();
+    allCountries = data;
     renderCountries(data);
   } catch (error) {
     console.log(error.message);
@@ -36,14 +38,14 @@ function clear() {
 function renderCountries(countries) {
   clear();
   countries.forEach((country) => {
-    console.log(country);
+    // console.log(country);
     countriesGrid.innerHTML += `
     <div class="country-card">
         <img src="${country.flags.png}" alt="${country.name.common}" />
         <div class="country-details">
             <h3 class="name"> ${country.name.common}</h3>
             <p id="population"><span>Population:</span> ${Number(
-                country.population
+              country.population
             ).toLocaleString()}</p>
             <p id="region"><span>Region:</span> ${country.region}</p>
             <p id="capital"><span>Capital:</span> ${country.capital[0]}</p>
@@ -51,5 +53,17 @@ function renderCountries(countries) {
     </div>`;
   });
 }
+
+dropdownOptions.addEventListener("click", (e) => {
+  let selectedRegion = e.target.closest("li").dataset.value;
+//   console.log(selectedRegion);
+//   console.log(allCountries);
+  if (selectedRegion) {
+    const filterdRegion = allCountries.filter(
+      (country) => country.region.toLowerCase() === selectedRegion
+    );
+    renderCountries(filterdRegion);
+  }
+});
 
 fetchCountries();
